54e4345398804448f3a4bd04b9e7af278aa4a157
AnkitaD <ankitadhyani.84@gmail.com>
2019-04-13 21:36:57 -0400

    homepage and youtube done

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+
+    // set up event listener for form submit to capture username/email-id 
+    $("#username-form").on("submit", function(event) {
+        event.preventDefault();
+
+        console.log("Playlist requested");
+
+        var username = $("#name-input").val().trim();
+
+        console.log("username: " + username);
+
+        //If username entered by the user is an empty string
+        if(username === "") {
+            $("#alertLabel")
+                .text("Please enter your Username / Email-Id !!!")
+                .css("color", "red");
+        }
+
+    });
+
+});
